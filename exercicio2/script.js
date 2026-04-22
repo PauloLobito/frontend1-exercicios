ffd7f7b500f09141aa3db036034cca71a886b797
Paulo Lobito <235763087+PauloLobito@users.noreply.github.com>
2026-04-22 18:08:57 +0000

    Exercicio2 Updated

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,29 +1,25 @@
-let nome = JSON.parse(localStorage.getItem("name"));
-console.log(nome);
-let nameInput = document.getElementById("name");
-let buttonToggleForm = localStorage.getItem("toggleForm");
-let theme =localStorage.getItem("theme"); 
-let themeButton = document.getElementById("theme-button");
+const formulario = document.getElementById("formulario");
 
-theme ? (themeButton.innerText = theme) : (themeButton.innerText = "Light Mode");
 
-themeButton.addEventListener("click", function() {
-    if (theme === "Light Mode") {
-        localStorage.setItem("theme", "Dark Mode");
-        theme = "Dark Mode";
-        themeButton.innerText = "Dark Mode";
-    } else {
-        localStorage.setItem("theme", "Light Mode");
-        theme = "Light Mode";
-        themeButton.innerText = "Light Mode";
+
+formulario.addEventListener("submit", function(e){
+    e.preventDefault();
+    const nome = document.getElementById("inputNome").value;
+    //SetItem = Guarda os valores na LocalStorage.
+    localStorage.setItem("Nome Guardado", nome);
+
+    console.log(nome)
+
+    document.getElementById("mensagem").textContent = "Nome Guardado" + nome;
+})
+
+window.addEventListener("load",function(){
+    const nomeGuardado = localStorage.getItem("nome_guardado");
+
+    if (nomeGuardado){
+        console.log("Nome recuperado na LocalStorage: ",nomeGuardado);
+        document.getElementById("inputNome").value = nomeGuardado;
+    }else{
+        console.log("Nenhum nome guardado ainda");
     }
 });
-
-
-
-
-buttonToggleForm.addEventListener("submit", function() {
-    localStorage.setItem("name", JSON.stringify(nameInput.value));
-});
-
-
