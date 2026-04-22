2e4d65a6fd19dbe253ec12b2c14fd2fe600f0ac9
Paulo Lobito <235763087+PauloLobito@users.noreply.github.com>
2026-04-22 18:23:47 +0000

    Exercicio2 Updated

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,25 +1,55 @@
 const formulario = document.getElementById("formulario");
+const btntema = document.getElementById("btnTema");
+const temaAtual = document.getElementById("TemaMostra");
 
 
 
-formulario.addEventListener("submit", function(e){
+formulario.addEventListener("submit", function (e) {
     e.preventDefault();
     const nome = document.getElementById("inputNome").value;
     //SetItem = Guarda os valores na LocalStorage.
-    localStorage.setItem("Nome Guardado", nome);
+    localStorage.setItem("nome_guardado", nome);
 
     console.log(nome)
 
-    document.getElementById("mensagem").textContent = "Nome Guardado" + nome;
+    document.getElementById("mensagem").textContent = "Nome Guardado: " + nome;
 })
 
-window.addEventListener("load",function(){
+window.addEventListener("load", function () {
     const nomeGuardado = localStorage.getItem("nome_guardado");
 
-    if (nomeGuardado){
-        console.log("Nome recuperado na LocalStorage: ",nomeGuardado);
+    if (nomeGuardado) {
+        console.log("Nome recuperado na LocalStorage: ", nomeGuardado);
         document.getElementById("inputNome").value = nomeGuardado;
-    }else{
+    } else {
         console.log("Nenhum nome guardado ainda");
     }
 });
+
+function aplicartema(tema) {
+    document.body.classList.remove("light", "dark");
+    document.body.classList.add(tema);
+    temaAtual.textContent = "Tema Atual: " + tema;
+}
+
+window.addEventListener("load", function () {
+    const temaGuardado = sessionStorage.getItem("tema");
+
+    if (temaGuardado) {
+        aplicartema(temaGuardado);
+    } else {
+        aplicartema("light");
+        sessionStorage.setItem("tema", "light");
+    }
+});
+
+btntema.addEventListener("click", function () {
+    const temaAtualValor = sessionStorage.getItem("tema") || "light";
+    const novoTema = temaAtualValor === "light" ? "dark" : "light";
+
+    sessionStorage.setItem("tema", novoTema);
+    aplicartema(novoTema);
+});
+
+
+
